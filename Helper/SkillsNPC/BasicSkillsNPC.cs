3a0b991d33ebe454860102532ad2bbcff8158d76
Ppayrus <tjk_18312185454@sina.com>
2023-08-21 10:21:05 +0800

    乱七八糟

---
+++ Helper/SkillsNPC/BasicSkillsNPC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSkill.Helper.SkillsNPC
+{
+    public abstract class BasicSkillsNPC : ModNPC, IBasicSkillsNPC
+    {
+    }
+}
