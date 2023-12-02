cc6f8b5d1e25cc75369f63e4b6b4b70714175d72
Ppayrus <tjk_18312185454@sina.com>
2023-12-03 05:16:22 +0800

    更新技能NPC基类

---
+++ Helper/SkillNPC/NPC_ModeClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSkill.Helper.SkillNPC
+{
+    /// <summary>
+    /// NPC的状态类
+    /// </summary>
+    public class NPC_ModeClass
+    {
+        /// <summary>
+        /// 技能的切换字典,通过当前传入的技能来读取技能的切换
+        /// </summary>
+        public Dictionary<BasicNPCSkill, List<BasicNPCSkill>> SkillDict;
+        /// <summary>
+        /// 技能的切换函数
+        /// </summary>
+        /// <param name="currentSkill"></param>
+        /// <returns>当返回 null 的时候,则此技能切换作废</returns>
+        public virtual BasicNPCSkill SkillChange(BasicNPCSkill currentSkill)
+        {
+            if (currentSkill != null && SkillDict.TryGetValue(currentSkill, out var SkillList))
+            {
+                foreach (BasicNPCSkill targetSkill in SkillList)
+                {
+                    if (currentSkill.SwitchCondition() && targetSkill.ActivationCondition() || targetSkill.CompulsionSwitchSkill(currentSkill))
+                    {
+                        return targetSkill;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Helper/SkillNPC/NPC_ModeProcessClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSkill.Helper.SkillNPC
+{
+    public class NPC_ModeProcessClass
+    {
+        /// <summary>
+        /// 状态储存类
+        /// </summary>
+        public List<NPC_ModeClass> ModeList;
+        public int CurrentModeIndex;
+        public NPC_ModeClass CurrentMode
+        {
+            get => ModeList[CurrentModeIndex];
+            set => CurrentModeIndex = ModeList.FindIndex(x => x == value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="currentSkill"></param>
+        /// <returns>返回false则说明技能切换失败</returns>
+        public bool ChangeSkill(BasicNPCSkill currentSkill)
+        {
+            return CurrentMode.SkillChange(currentSkill) != null;
+        }
+    }
+}
